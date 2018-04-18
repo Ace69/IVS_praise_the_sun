3dde88eb38d0296ad0ea83e44829bf81c8f02bdd
xsvera04 <xsvera04@fit.vutbr.cz>
2018-04-18 21:33:24 +0200

    Doplneni hlavicky a doplneni diakritiky u komentaru.

--- src/src/Profiling/Profiling.java
+++ src/src/Profiling/Profiling.java
@@ -1,10 +1,33 @@
+/****************************************************************************
+* Název projektu: Kalkulaèka                                                *
+* Balíèek: Profiling                                                        *
+* Soubor: Profiling.java                                                    *
+* Datum: 7.4.2018                                                           *
+* Autor: David Bulawa(xbulaw01)                                             *
+*                                                                           *
+* Popis: Profiling - výpoèet smìrodatné odchylky pomocí mat. knihovny.      *
+*                                                                           *
+****************************************************************************/
+
+/**
+ * @file Profiling.java 
+ * 
+ * @brief Profiling matematické knihovny.
+ */
+
+/**
+ * @package Profiling
+ * 
+ * @brief Profiling matematické knihovny.
+ */
 package Profiling;
 
 import Lib.MathLib;
 import java.util.ArrayList;
 import java.util.Scanner;
+
 /**
- * @brief Trida pro vypocet smerodatne odchylka
+ * @brief Tøída pro výpoèet smìrodatné odchylky
  * @author David Bulawa(xbulaw01)
  * @date 7.4.2018
  * @version 1.0
@@ -12,44 +35,43 @@ import java.util.Scanner;
 public class Profiling {
     
     /**
-     * @brief Funkce pro vypocet Aritmetickeho prumeru pole.
-     * @param pole Pole nad kterym prumernou hodnotu pocitame.
-     * @return Vraci prumernou hodnotu prvku v poli.
+     * @brief Funkce pro výpoèet aritmetického prùmìru pole.
+     * @param pole Pole nad kterým prùmìrnou hodnotu poèítáme.
+     * @return Vrací prùmìrnou hodnotu prvkù v poli.
      */
     public static double prumer(ArrayList<Double> pole){
        double prumer = 0;
-       //Secte vsechny prvky v poli
+       //Seète všechny prvky v poli
        for(int i = 0; i < pole.size();i++){
          prumer = MathLib.scitani(prumer, pole.get(i));
        }
-       //Vydelime poctem
+       //Vydìlíme poètem
        return MathLib.deleni(prumer, pole.size());
     }
+    
     /**
-     * Funkce pro vypocet Smerodatne odchylky prvku v poli.
-     * @param pole Pole nad kterym smerodatnou odchylku pocitame.
-     * @return Vraci smerodatnou odchylku prvku v poli.
+     * @brief Funkce pro výpoèet smìrodatné odchylky prvkù v poli.
+     * @param pole Pole nad kterým smìrodatnou odchylku poèítáme.
+     * @return Vrací smìrodatnou odchylku prvkù v poli.
      */
     public static double smOdchylka(ArrayList<Double> pole){
         double rada = 0;
-        //Do promene rada ukladame sumu prvku v poli^2
+        //Do promìnné øada ukládáme sumu prvkù v poli^2
         for (int i = 0; i < pole.size(); i++) {
             rada += MathLib.mocnina(pole.get(i), 2);
         }
         double tmp = 0;
-        //do promene tmp si ulozime Pocet prvku * prumer pole^2
+        //Do promìnné tmp si uložíme poèet prvkù * prùmìr pole^2
         tmp = MathLib.nasobeni(pole.size(), MathLib.mocnina(prumer(pole), 2));
-        //tyto hodnoty od sebe odecteme
+        //Tyto hodnoty od sebe odeèteme
         tmp = rada - tmp;
-        //nakonec prinasobime 1/PocetPrvku - 1 a cele to umocnime
+        //Nakonec pøinásobíme 1/PoèetPrvkù - 1 a celé to umocníme
         return MathLib.odmocnina(MathLib.nasobeni(MathLib.deleni(1,MathLib.odcitani(pole.size(), 1)), tmp), 2);
     }
+    
     /**
-     * @brief Main zatim slouzi k otestovani.
-     * Program by mel pocitat spravne.
-     * @param args 
-     * @TODO Predelat na automatickou detekci poctu cisel a poresit cteni cisel ze stdin(mozna funguje) viz. zadani.
-     * @TODO Prodiskutovat.
+     * @brief Vypíše výsledek výpoètu smìrodatné odchylky posloupnosti èísel.
+     * @param args Argumenty pøíkazové øádky 
      */
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -64,4 +86,4 @@ public class Profiling {
         
         System.out.println(smOdchylka(list));
     }
-}
+} // public class Profiling
